490aaa71150a6b2ae464a830802d37504f71e70c
infinityc2 <super_star@hotmail.co.th>
2019-08-13 17:48:21 +0700

    close #13 register teacher

---
+++ src/components/teacher/system/RegYear.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-card class="elevation-3 mx-auto">
+      <v-card-title>ข้อมูลปีการศึกษา</v-card-title>
+      <v-card-text>
+        <v-layout column wrap align-start d-flex>
+          <v-flex xs12 sm6 md4>
+            <Seat :dialog="dialog"></Seat>
+          </v-flex>
+        </v-layout>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import Seat from './parts/Seat'
+
+export default {
+    components: {
+        Seat
+    },
+    data () {
+        return {
+            dialog: null
+        }
+    }
+}
+</script>
